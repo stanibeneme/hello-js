e8f457a921e85740652b687c4a58f8f5b4a73b3c
stanley <stanleyibeneme92@gmail.com>
2026-02-15 22:49:34 +0100

    assignment name, age, favourite programming language

---
+++ intro.js
@@ -0,0 +1,9 @@
+const name = 'Stanley';
+const age = 33;
+const favourite = 'python';
+
+console.log(name);
+console.log(age);
+console.log(favourite);
+
+console.log(`My name is ${name}, I am ${age} years old, and I love ${favourite}.`);
